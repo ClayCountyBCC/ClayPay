811dd6dfac538eb43c7303b7742691420d7d7bfc
Jeremy West <jeremy.west@claycountygov.com>
2018-04-20 16:58:11 -0400

    Balancing controller and Models needed to process payments and create DJournal

---
+++ ClayPay/Controllers/Balancing/BalancingController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ClayPay.Models.Balancing;
+
+namespace ClayPay.Controllers
+{
+  public class BalancingController : ApiController
+  {
+
+    // returns from functions Account.GetGLAccountTotalsGetDJournal, payment.process, payment.getgutotals, 
+    // and payment.getallpayments() are all Pre-Formatted.
+    // The Printed DJournal example is in Queries\WATSC\ClayPay\Balancing
+
+    /****    DJournal Format  *****/
+
+    // Date
+    // Clay County, BCC
+    // 
+    // Total Charges   $000,000.00  // total charges is TotalPayments is Payment.Process.Last()
+    // _____________   ___________
+    // display Payment.Process
+
+    // =============   ===========
+    // Total Payments  $000,000.00  Total Deposit   $000,000.00
+    // ______________  ___________
+    // display Payment.GetGUTotals
+
+    // display Account.GetAccountTotals
+
+
+    public IHttpActionResult GetDJournal(DateTime DateToBalance)
+    {
+      List<Account> DjournalEntries =  Account.GetGLAccountTotals(DateToBalance);
+      if (DjournalEntries == null)
+      {
+        return InternalServerError();
+      }
+      else
+      {
+        return Ok(DjournalEntries);
+      }
+    }
+
+    public IHttpActionResult Process(DateTime DateToBalance)
+    {
+      List<Payment> paymentTypeTotals = Payment.Process(DateToBalance);
+      if (paymentTypeTotals == null)
+      {
+        return InternalServerError();
+      }
+      else
+      {
+        return Ok(paymentTypeTotals);
+      }
+    }
+
+    public IHttpActionResult GetGUTotals(DateTime DateToBalance)
+    {
+      List<Payment> GUTotals = Payment.GetGUTotals(DateToBalance);
+      if (GUTotals == null)
+      {
+        return InternalServerError();
+      }
+      else
+      {
+        return Ok(GUTotals);
+      }
+    }
+
+    // Return All Payments made on DateToBalance
+    // Use client side to filter if needed.
+    public IHttpActionResult GetAllPayments(DateTime DateToBalance)
+    {
+      List<Payment> Payments = Payment.GetAllCashierIdTotals(DateToBalance);
+      if (Payments == null)
+      {
+        return InternalServerError();
+      }
+      else
+      {
+        return Ok(Payments);
+      }
+    }
+  }
+}
---
+++ ClayPay/Models/Balancing/Payment.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Dapper;
+
+namespace ClayPay.Models.Balancing
+{
+  public class Payment
+  {
+    public string OTid { get; set; }
+    public string CashierId { get; set; }
+    public double AmtApplied { get; set; }
+    public double AmtTendered { get; set; }
+    public double Total { get; set; }
+    public string Type { get; set; }
+    public string Code { get; set; }
+
+    public Payment()
+    {
+      
+    }
+
+    public static List<Payment> Process(DateTime DateToBalance)
+    {
+      var dbArgs = new DynamicParameters();
+
+      dbArgs.Add("@DateToBalance", DateToBalance);
+
+      var sql = @"
+      USE WATSC;
+
+      --SELECT * CashierIds FROM DATE
+      WITH CashierIdsToBalance (CashierId) AS (
+      SELECT CashierId
+      FROM dbo.ccCashier C
+      WHERE CAST(TransDt AS DATE) = CAST(@DateToBalance AS DATE))
+      -- SELECT * Payment Types FROM ccLOOKUP TABLE
+      ,PaymentTypes (Code, SortKey) AS (
+      SELECT DISTINCT Code,SortKey
+      FROM ccLookUp L
+      WHERE CdType = 'PMTTYPE')
+
+      -- CREATE TMP TABLE TO BE ABLE TO MERGE ALL TOTALS TO ONE WORKABLE TABLE
+      SELECT Code, AmtApplied FROM (
+      SELECT L.SortKey,L.Code, SUM(AmtApplied) AmtApplied
+      FROM (
+      SELECT PmtType, AmtApplied
+      FROM  ccCashierPayment CP
+      LEFT OUTER join ccCashier C ON C.OTId = CP.OTid
+      WHERE CashierId IN (SELECT CashierId FROM CashierIdsToBalance)) AS TMP
+      RIGHT OUTER JOIN ccLookUp L ON LEFT(TMP.PmtType,5) = LEFT(L.CODE,5)
+      WHERE L.CdType = 'PMTTYPE'
+      GROUP BY L.SORTKEY, L.Code, PmtType
+      UNION
+      SELECT 98 SortKey, 'Change' Code, (SUM(AMTAPPLIED) - SUM(AMTTENDERED)) * (-1)
+      FROM ccCashierPayment CP
+      inner join ccCashier C ON C.OTId = CP.OTid
+      WHERE CashierId IN (SELECT CashierId FROM CashierIdsToBalance)
+        AND PmtType IN ('CK', 'CA')
+      UNION 
+      SELECT 99 SortKey, 'Total Deposit' Code, SUM(AmtApplied)
+      FROM ccCashierPayment CP
+      inner join ccCashier C ON C.OTId = CP.OTid
+      WHERE CashierId IN (SELECT CashierId FROM CashierIdsToBalance)
+        AND PmtType IN ('CK', 'CA')
+      UNION 
+      SELECT 100 SortKey, 'Total Payments' Code, SUM(AmtApplied)
+      FROM ccCashierPayment CP
+      INNER JOIN ccCashier C ON CP.OTid = C.OTid
+      WHERE CashierId IN (SELECT CashierId FROM CashierIdsToBalance)
+      ) AS TMP
+      ORDER BY SortKey;
+
+       ";
+      try
+      {
+        
+        return Constants.Get_Data<Payment>(sql, dbArgs);
+      }
+      catch (Exception ex)
+      {
+        Constants.Log(ex, sql);
+        return new List<Payment>();
+      }
+    }
+
+    public static List<Payment> GetGUTotals(DateTime DateToBalance)
+    {
+      var dbArgs = new DynamicParameters();
+
+      dbArgs.Add("@DateToBalance", DateToBalance);
+
+      var sql = @"
+      USE WATSC;
+
+      SELECT Case WHEN Type = 'd' THEN 'GU Debit' ELSE 'GU Credit' END [TYPE], sum(amount) amount from (
+       SELECT otid, cashierid, cast(transdt AS DATE) transdt, account, amount, type
+      FROM dbo.ccGU INNER JOIN dbo.ccGUItem ON dbo.ccGU.GUId = dbo.ccGUItem.GUID
+       WHERE CAST(TransDt AS DATE) = CAST(@DateToBalance AS DATE)) AS tmp
+       GROUP BY type
+       ORDER BY Type DESC
+
+       ";
+      try
+      {
+
+        return Constants.Get_Data<Payment>(sql, dbArgs);
+      }
+      catch (Exception ex)
+      {
+        Constants.Log(ex, sql);
+        return new List<Payment>();
+      }
+    }
+
+    public static List<Payment> GetAllCashierIdTotals(DateTime DateToBalance)
+    {
+      var dbArgs = new DynamicParameters();
+
+      dbArgs.Add("@DateToBalance", DateToBalance);
+
+      var sql = @"
+      USE WATSC;
+
+      WITH ALL_CHOSEN (CashierId , OTid, AmtApplied) AS (
+      SELECT DISTINCT CI.CashierId ,CP.OTid, AmtApplied
+      FROM ccCashierPayment CP
+      LEFT OUTER JOIN ccLookUp L ON LEFT(CP.PmtType,5) = LEFT(L.Code,5)
+      LEFT OUTER JOIN ccCashierItem CI ON CP.OTid = CI.OTId
+      LEFT OUTER JOIN ccCatCd CC ON CC.CatCode  = CI.CatCode
+      LEFT OUTER JOIN CCCASHIER C ON CI.CashierId = C.CashierId
+      WHERE CAST(C.TransDt AS DATE) = CAST(@DateToBalance AS DATE)
+        AND Description IS NOT NULL
+        AND TOTAL IS NOT NULL 
+      ),otidTotals (otid, total) as (
+      select otid, sum(amttendered) total
+      from ccCashierPayment cp
+      GROUP BY OTid)
+      
+      SELECT AC.CashierId, AC.OTid,AC.AmtApplied, OT.total FROM ALL_CHOSEN AC
+      INNER JOIN otidTotals OT ON OT.otid = AC.OTid
+      UNION
+      SELECT NULL,NULL,(SELECT SUM(AmtApplied) FROM ALL_CHOSEN),NULL
+      ORDER BY AmtApplied
+
+       ";
+      try
+      {
+
+        return Constants.Get_Data<Payment>(sql, dbArgs);
+      }
+      catch (Exception ex)
+      {
+        Constants.Log(ex, sql);
+        return new List<Payment>();
+      }
+    }
+  }
+}
