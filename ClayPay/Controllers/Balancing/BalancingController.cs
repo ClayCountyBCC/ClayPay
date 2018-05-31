7c5eff1046e1aa425d1b42131bc38ea98978b884
Jeremy West <jeremy.west@claycountygov.com>
2018-05-31 17:28:08 -0400

    update comments and Save Route

--- ClayPay/Controllers/Balancing/BalancingController.cs
+++ ClayPay/Controllers/Balancing/BalancingController.cs
@@ -15,8 +15,19 @@ namespace ClayPay.Controllers
   public class BalancingController : ApiController
   {
     /**************************************************************************************************************
-     returns from functions Account.GetGLAccountTotalsGetDJournal, payment.process, payment.getgutotals, 
-     and payment.getallpayments() are all Pre-Formatted.
+     Both Get and Save routes Return a DJournal
+
+     Parameters: 
+        Get: DateToBalance (DateTime.Date)
+          1. Can be used to view any DJournal Date.
+          2. Can only reprint DJournals that have been finalized
+
+        Post: DateToFinalize (DateTime.Date)
+          1. Can only finalize DJournal for payments made before today.
+          2. Can only finalize DJournal for single date.
+            * If DateToFinalize < today then finalize = true 
+            * Else finalize = false
+         
      The Printed DJournal example is in Queries\WATSC\ClayPay\Balancing
 
     ****    DJournal Format  **** 
@@ -54,14 +65,13 @@ namespace ClayPay.Controllers
       }
     }
 
-
     [HttpPost]
     [Route("Save")]
     public IHttpActionResult Save(DateTime DateToFinalize)
     {
       try
       { 
-        var dj = new DJournal(DateToFinalize, (DateToFinalize.Date < DateTime.Now.Date ? true : false), User.Identity.Name);
+        var dj = new DJournal(DateToFinalize, DateToFinalize.Date < DateTime.Now.Date, User.Identity.Name);
         dj.Error.Add("Cannot finalize payments made on or after today. Please select a previous date.");
         return Ok(dj);
       }
@@ -71,63 +81,6 @@ namespace ClayPay.Controllers
         return InternalServerError();
       }
     }
-
-    //public IHttpActionResult GetDJournal(DateTime DateToBalance)
-    //{
-    //  // TODO: Link getting djournal to UserAccess when it is applied
-    //  var dj = new DJournal(DateToBalance);
-    //  if (newDjournal == null)
-    //  {
-    //    return BadRequest("Error returning DJournal. Please contact the help desk if this issue persists.");
-    //  }
-    //  else
-    //  {
-    //    return Ok(newDjournal);
-    //  }
-    //}
-
-    //// Return All Payments made on DateToBalance
-    //// Use client side to filter if needed.
-    //public IHttpActionResult GetAllPayments(DateTime DateToBalance, string paymentType = "")
-    //{
-    //  List<CashierTotal> Payments = Payment.GetPayments(DateToBalance, paymentType);
-    //  if (Payments == null)
-    //  {
-    //    return BadRequest("Error returning payments. Please contact the help desk if this issue persists.");
-    //  }
-    //  else
-    //  {
-    //    return Ok(Payments);
-    //  }
-    //}
-
-
-    //public IHttpActionResult Process(DateTime DateToBalance)
-    //{
-    //  List<CashierTotal> paymentTypeTotals = CashierTotal.Process(DateToBalance);
-    //  if (paymentTypeTotals == null)
-    //  {
-    //    return BadRequest("Error processing payment data. Please contact the help desk if this issue persists.");
-    //  }
-    //  else
-    //  {
-    //    return Ok(paymentTypeTotals);
-    //  }
-    //}
-
-    //public IHttpActionResult GetGUTotals(DateTime DateToBalance)
-    //{
-    //  List<CashierTotal> GUTotals = CashierTotal.GetGUTotals(DateToBalance);
-    //  if (GUTotals == null)
-    //  {
-    //    return BadRequest("Error returning GU totals. Please contact the help desk if this issue persists.");
-    //  }
-    //  else
-    //  {
-    //    return Ok(GUTotals);
-    //  }
-    //}
-
 
   }
 }
