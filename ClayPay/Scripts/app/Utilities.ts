0e39d59a9d1e705533fd79fb9293dab9479db4ee
Jeremy West <jeremy.west@claycountygov.com>
2018-08-22 16:18:23 -0400

    update to include ie error

--- ClayPay/Scripts/app/Utilities.ts
+++ ClayPay/Scripts/app/Utilities.ts
@@ -392,5 +392,33 @@ namespace Utilities
   //  Container.appendChild(innerContainer);
   //  return Container;
   //}
-
+  export function CheckBrowser()
+  {
+    let browser: string = "";
+    if ((navigator.userAgent.indexOf("Opera") || navigator.userAgent.indexOf('OPR')) != -1) 
+    {
+      browser = 'Opera';
+    }
+    else if (navigator.userAgent.indexOf("Chrome") != -1)
+    {
+      browser = 'Chrome';
+    }
+    else if (navigator.userAgent.indexOf("Safari") != -1)
+    {
+      browser = 'Safari';
+    }
+    else if (navigator.userAgent.indexOf("Firefox") != -1) 
+    {
+      browser = 'Firefox';
+    }
+    else if ((navigator.userAgent.indexOf("MSIE") != -1) || (!!document.DOCUMENT_NODE == true)) //IF IE > 10
+    {
+      browser = 'IE';
+    }
+    else 
+    {
+      browser = 'unknown';
+    }
+    return browser;
+  }
 }
