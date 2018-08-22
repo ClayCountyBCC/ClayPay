75eb8434ad49ff5904d200638df1d1ebfd5ae035
Daniel McCartney <daniel.mccartney@claycountygov.com>
2018-08-22 16:44:05 -0400

    Updated IE detection.

--- ClayPay/Scripts/app/Utilities.js
+++ ClayPay/Scripts/app/Utilities.js
@@ -284,5 +284,29 @@ var Utilities;
     //  Container.appendChild(innerContainer);
     //  return Container;
     //}
+    function CheckBrowser() {
+        var browser = "";
+        if ((navigator.userAgent.indexOf("Opera") || navigator.userAgent.indexOf('OPR')) != -1) {
+            browser = 'Opera';
+        }
+        else if (navigator.userAgent.indexOf("Chrome") != -1) {
+            browser = 'Chrome';
+        }
+        else if (navigator.userAgent.indexOf("Safari") != -1) {
+            browser = 'Safari';
+        }
+        else if (navigator.userAgent.indexOf("Firefox") != -1) {
+            browser = 'Firefox';
+        }
+        else if ((navigator.userAgent.indexOf("MSIE") != -1) || (!!document.DOCUMENT_NODE == true)) //IF IE > 10
+         {
+            browser = 'IE';
+        }
+        else {
+            browser = 'unknown';
+        }
+        return browser;
+    }
+    Utilities.CheckBrowser = CheckBrowser;
 })(Utilities || (Utilities = {}));
 //# sourceMappingURL=Utilities.js.map
